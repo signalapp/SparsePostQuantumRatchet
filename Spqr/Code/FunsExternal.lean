497a2e5360ea4e7e37ce0af4437f253674df30dc
Hoang Le Truong <40373735+truonghoangle@users.noreply.github.com>
2026-05-28 09:03:07 +0200

    Fix axiom encoding.gf.GF16.Insts.CoreOpsArithMulAssignShared0GF16.mul_assign in gf.rs (#79)
    This PR is to replace the `mul_assign` axiom with a definition. This is
    currently blocked because it depends on `unaccelerated::mul`, which is
    generated after related functions due to function ordering. Reordering
    the Rust functions should resolve the issue.
    
    closes #73

--- Spqr/Code/FunsExternal.lean
+++ Spqr/Code/FunsExternal.lean
@@ -1994,11 +1994,6 @@ axiom kdf.hkdf_to_slice
   Slice Std.U8 → Slice Std.U8 → Slice Std.U8 → Slice Std.U8 → Result
     (Slice Std.U8)
 
-/-- [spqr::encoding::gf::{core::ops::arith::MulAssign<&0 (spqr::encoding::gf::GF16)> for spqr::encoding::gf::GF16}::mul_assign]:
-    Source: 'src/encoding/gf.rs', lines 127:4-137:5 -/
-axiom encoding.gf.GF16.Insts.CoreOpsArithMulAssignShared0GF16.mul_assign
-  : encoding.gf.GF16 → encoding.gf.GF16 → Result encoding.gf.GF16
-
 /-- [spqr::encoding::gf::mul2_u16]:
     Source: 'src/encoding/gf.rs', lines 216:0-225:1 -/
 axiom encoding.gf.mul2_u16
